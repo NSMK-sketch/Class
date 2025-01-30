2fc7ddeb64f7b5ef101d5070712f63b033b4df1f
prep <prep@DESKTOP-O69D4LJ>
2025-01-30 11:41:43 +0700

    Добавьте файлы проекта.

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class BankAccount
+    {
+        private static int s_accountNumberSeed = 1234567890;
+        public BankAccount(string name, decimal initialBalance) 
+        { 
+            this.Owner = name;
+            this.Balance = initialBalance;
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+        }
+        private List<Transaction> _allTransactions = new List<Transaction>();
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            _allTransactions.Add(withdrawal);
+        }
+
+        public void MakeWithDeposit(decimal amount, 
+            DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ Classes/CodeFile1.cs
@@ -0,0 +1,4 @@
+﻿using Classes;
+
+var account = new BankAccount("alex", 1000);
+Console.WriteLine($"Акаунт {account.Number} Создан {account.Owner} на нем {account.Balance} баланс.");
